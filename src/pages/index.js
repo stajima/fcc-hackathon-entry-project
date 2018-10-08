cc8ee277879ce0565cac355b5b79f4e4e541a2b3
Vikram Raja <vikramraja1995@users.noreply.github.com>
2018-10-07 19:00:55 -0700

    Set up basic Hello World page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,6 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => <h1>Hello World!</h1>
 
 export default IndexPage
